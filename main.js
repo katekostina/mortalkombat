b0d60553445528ac8bbcfe12eda711bf29eb6cbd
Kate Kostina <katekostina@yahoo.com>
2021-06-20 22:39:21 +0300

    homework-9. required part

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import { PLAYER1, PLAYER2, ATTACK, HIT } from "./constants";
-
 import Game from './Game';
-export const game = new Game({ PLAYER1, PLAYER2, ATTACK, HIT });
+
+export const game = new Game();
 
 game.start();
